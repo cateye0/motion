7424cc906e0a8c38f3b9ef4b68a087539e9d508e
cateye0 <cateye@cateye.hu>
2020-02-24 22:19:33 +0100

    Update motion_restart.sh

--- motion_restart.sh
+++ motion_restart.sh
@@ -12,19 +12,6 @@ LOG="/tmp/motion.log"
 VAR1="!!! MOTION RESTARTED BY CRONTAB !!!"
 VAR2=`date '+%Y-%m-%d %H:%M:%S'`
 
-# find myprog pid
-#$PGREP ${MYPROG}
-# if not running
-#if [ $? -ne 0 ]
-#then
-#   $STOP
-#   sleep 10
-#   $START
-#   echo -e $VAR1 >> $LOG
-#   echo -e $VAR2 >> $LOG
-#fi
-
-
 if ps -ef | grep -v grep | grep /usr/bin/motion ; then
         exit 0
 else
